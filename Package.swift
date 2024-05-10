1a994063c73a7a0c0239ea2d56dd6330ed2c16e8
Amin-0-1 <mahmoudamin373@gmail.com>
2024-05-11 01:59:28 +0300

    cleanup

--- Package.swift
+++ Package.swift
@@ -7,14 +7,12 @@ let package = Package(
     name: "NetworkManager",
     platforms: [.iOS(.v13), .macOS(.v11)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "NetworkManager",
             targets: ["NetworkManager"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
+
         .binaryTarget(
             name: "NetworkManager",
             url: "https://github.com/Amin-0-1/NetworkManager/releases/download/v1.0.0/NetworkManager.xcframework.zip",
