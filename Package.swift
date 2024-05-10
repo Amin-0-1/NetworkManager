6e80a27d9cd3fce19c0e346aaa2b2179369bead0
Amin-0-1 <mahmoudamin373@gmail.com>
2024-05-10 16:19:51 +0300

    add: framework

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "NetworkManager",
+    platforms: [.iOS(.v13), .macOS(.v11)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -14,8 +15,10 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "NetworkManager"),
+        .binaryTarget(
+            name: "NetworkManager",
+            path: "./Sources/NetworkManager.xcframework"
+        ),
         .testTarget(
             name: "NetworkManagerTests",
             dependencies: ["NetworkManager"]),
