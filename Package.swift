c0504197dd59f452d82e51a280fd483c991e028d
Amin-0-1 <m.amin@moddakir.com>
2024-05-28 17:36:15 +0300

    bump 1.3.3

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
 
         .binaryTarget(
             name: "NetworkManager",
-            url: "https://github.com/Amin-0-1/NetworkManager/releases/download/1.3.2/NetworkManager.xcframework.zip",
-            checksum: "00e0dd5e1e312c926e2c9565af14bc648d2c5785eeba4ae9c0a0234fe94cf50c"
+            url: "https://github.com/Amin-0-1/NetworkManager/releases/download/1.3.3/NetworkManager.xcframework.zip",
+            checksum: "1c85ac0bdfe07d7fad6e1ff8d27486e067ab7d024668e9ca731612b98e469630"
         ),
     ]
 )
