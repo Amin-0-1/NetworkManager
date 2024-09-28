799b105d57fdd530c5ce8f55d9e0395d2ad8ce4f
Amin-0-1 <m.amin@moddakir.com>
2024-09-28 05:02:43 +0300

    update: add Resources on target

--- Package.swift
+++ Package.swift
@@ -17,7 +17,8 @@ let package = Package(
     targets: [
         .target(
             name: "NetworkManager",
-            path: "Sources/Network"
-        ),
+            path: "Sources/Network",
+            resources: [.process("Resources")]
+        )
     ]
 )
